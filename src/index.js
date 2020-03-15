661532906032e4227b53f989f116dac003087415
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-03-16 00:31:50 +0900

    improve code, wrong string value do not show debug log

--- src/index.js
+++ src/index.js
@@ -4,15 +4,19 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
   console.log(`${count} / ${action}`);
   // if count is undefined, default is 0
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
@@ -20,16 +24,16 @@ const countStore = createStore(countModifier); // should need function ( as redu
 
 const onChange = () => {
   number.innerText = countStore.getState();
-}
+};
 
 countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: ADD });
 };
 
 const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 };
 
 add.addEventListener("click", handleAdd);
