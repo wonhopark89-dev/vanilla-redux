0ba6b9059089b3580960336e169587dfa26c16b5
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-03-16 23:53:43 +0900

    vanilla ja + redux, remember ! do not mutate state when return state

--- src/index.js
+++ src/index.js
@@ -1,40 +1,76 @@
 import { createStore } from "redux";
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-const ADD = "ADD";
-const MINUS = "MINUS";
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
-const countModifier = (count = 0, action) => {
-  console.log(`${count} / ${action}`);
-  // if count is undefined, default is 0
+const addToDo = text => {
+  return {
+    type: ADD_TODO,
+    text
+  };
+};
+
+const deleteToDo = id => {
+  return {
+    type: DELETE_TODO,
+    id
+  };
+};
+
+const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
+    case ADD_TODO:
+      // return state.push(action.text) // never mutate state !!!
+      // return [...state, { text: action.text, id: Date.now() }]; // it is correct
+      return [{ text: action.text, id: Date.now() }, ...state]; // it is correct ( order )
+    case DELETE_TODO:
+      return state.filter(toDo => toDo.id !== action.id);
     default:
-      return count;
+      return state;
   }
 };
+const store = createStore(reducer);
 
-const countStore = createStore(countModifier); // should need function ( as reducer );
+store.subscribe(() => console.log(store.getState()));
 
-const onChange = () => {
-  number.innerText = countStore.getState();
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = ""; // because render ?
+  toDos.forEach(toDo => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+
+    btn.innerText = "DEL";
+    btn.addEventListener("click", dispatchDeleteToDo);
+
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
 };
 
-countStore.subscribe(onChange);
+store.subscribe(paintToDos);
 
-const handleAdd = () => {
-  countStore.dispatch({ type: ADD });
+const dispathAddToDo = text => {
+  store.dispatch(addToDo(text));
 };
 
-const handleMinus = () => {
-  countStore.dispatch({ type: MINUS });
+const dispatchDeleteToDo = e => {
+  // console.log(e.target.parentNode.id); // 삭제할 부모 노드
+  const id = parseInt(e.target.parentNode.id); // 왜냐하면 HTML 로 부터 넘어오는 값은 string 이니까
+  store.dispatch(deleteToDo(id));
+};
+const onSubmit = e => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  dispathAddToDo(toDo);
 };
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+form.addEventListener("submit", onSubmit);
